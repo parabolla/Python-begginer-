0e8c7df70f0e57423fff3bb914311d0553b7eeee
Iliya Tkachev <xarament@gmail.com>
2022-10-04 00:46:58 +0400

    Completed hw4_normal#2,3

--- lessson 4/hw04_normal.py
+++ lessson 4/hw04_normal.py
@@ -26,15 +26,80 @@ fibonacci(1, three)
 
 
 def sort_to_max(origin_list):
-    pass
+    N = len(origin_list)  # число элементов в списке
+
+    for i in range(1, N):
+        for j in range(i, 0, -1):
+            if origin_list[j] < origin_list[j - 1]:  # если число больше второго, меняем местами
+                origin_list[j], origin_list[j - 1] = origin_list[j - 1], origin_list[j]
+            else:
+                break
+        # print(origin_list)
+    return origin_list
 
 
 sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
 
+
 # Задача-3:
 # Напишите собственную реализацию стандартной функции filter.
 # Разумеется, внутри нельзя использовать саму функцию filter.
+def sort_to_max(origin_list):
+    list_sort = 0
+    list_help = 0
+    list_two = []
 
+    for i in origin_list:
+        list_help = i
+        for b in origin_list:
+            if b >= i:
+                list_sort = i
+            elif b > list_help:
+                break
+            else:
+                list_help = b
+        list_two.append(list_help)
+        origin_list.remove(list_help)
+        list_help = 0
+
+    # print(list_two)
+    # print(origin_list)
+
+    for i in origin_list:
+        list_help = i
+        for b in origin_list:
+            if b >= i:
+                list_sort = i
+            elif b > list_help:
+                break
+            else:
+                list_help = b
+        list_two.append(list_help)
+        origin_list.remove(list_help)
+        list_help = 0
+
+    for i in origin_list:
+        list_help = i
+        for b in origin_list:
+            if b >= i:
+                list_sort = i
+            elif b > list_help:
+                break
+            else:
+                list_help = b
+        list_two.append(list_help)
+        origin_list.remove(list_help)
+        list_help = 0
+
+    list_return = list_two + origin_list
+    #
+    # print(list_two)
+    # print(origin_list)
+    # print(list_return)
+    return list_return
+
+
+sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
 
 # Задача-4:
 # Даны четыре точки А1(х1, у1), А2(x2 ,у2), А3(x3 , у3), А4(х4, у4).
