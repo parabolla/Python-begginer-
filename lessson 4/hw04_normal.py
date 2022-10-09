e2e937afc38f595844ac770772c621894155f36f
Iliya Tkachev <xarament@gmail.com>
2022-10-10 00:59:31 +0400

    completed hw4_normal. #3

--- lessson 4/hw04_normal.py
+++ lessson 4/hw04_normal.py
@@ -19,13 +19,14 @@ def fibonacci(n, m):
 three = int(input("Введите m элемент :"))
 print(fibonacci(1, three))
 
-
 # Задача-2:
 # Напишите функцию, сортирующую принимаемый список по возрастанию.
 # Для сортировки используйте любой алгоритм (например пузырьковый).
 # Для решения данной задачи нельзя использовать встроенную функцию и метод sort()
 
 1  #
+
+
 def sort_to_max(origin_list):
     N = len(origin_list)  # число элементов в списке
 
@@ -41,6 +42,7 @@ def sort_to_max(origin_list):
 
 2  #
 sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
+
 
 def sort_to_max(origin_list):
     list_sort = 0
@@ -104,11 +106,22 @@ sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
 # Напишите собственную реализацию стандартной функции filter.
 # Разумеется, внутри нельзя использовать саму функцию filter.
 
-def text_ot_int(fil)
-    pass
+def my_fil(fil):
+    text = []
+    numbers = []
+
+    for i in fil:
+        if isinstance(i, str):
+            text.append(i)
+        elif isinstance(i, int):
+            numbers.append(i)
+        else:
+            print(i)
+
+    return text, numbers
 
 
-fil = [1, "text", 3, 4, "text2", 6, "text3"]
+print(my_fil([1, "text", 3, 4, "text2", 6, "text3"]))
 
 # Задача-4:
 # Даны четыре точки А1(х1, у1), А2(x2 ,у2), А3(x3 , у3), А4(х4, у4).
