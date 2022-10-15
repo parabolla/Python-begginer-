db67763ea27748f130548f87f00b8df34850420f
Iliya Tkachev <xarament@gmail.com>
2022-10-15 18:33:41 +0400

    completed hw7_easy. #1.2

--- lesson 7/hw07_easy.py
+++ lesson 7/hw07_easy.py
@@ -26,3 +26,53 @@ print(t1.calculation_p())
 # Предусмотреть в классе методы:
 # проверка, является ли фигура равнобочной трапецией;
 # вычисления: длины сторон, периметр, площадь.
+import math
+
+
+class Trap:
+    def __init__(self, one, two, three, four, p=0, s=0):
+        self.one = one
+        self.two = two
+        self.three = three
+        self.four = four
+        self.p = p
+        self.s = s
+
+    def proverka_if(self):
+        c = math.sqrt(((self.two - self.one) ** 2) + ((self.two - self.one) ** 2))
+        d = math.sqrt(((self.four - self.three) ** 2) + ((self.four - self.three) ** 2))
+
+        if c == d:
+            print("Трапеция равнобокая")
+        else:
+            print("Трапеция неравнобокая")
+
+    def side(self):
+        c = math.sqrt(((self.two - self.one) ** 2) + ((self.two - self.one) ** 2))
+        d = math.sqrt(((self.four - self.three) ** 2) + ((self.four - self.three) ** 2))
+        a = math.sqrt(((self.three - self.two) ** 2) + ((self.three - self.two) ** 2))
+        b = math.sqrt(((self.four - self.one) ** 2) + ((self.four - self.one) ** 2))
+        print("Длина сторон: " + "\nAB: ", a, "\nBC: ", c, "\nCD: ", d, "\nDC: ", b)
+
+    def perimeter(self):
+        c = math.sqrt(((self.two - self.one) ** 2) + ((self.two - self.one) ** 2))
+        d = math.sqrt(((self.four - self.three) ** 2) + ((self.four - self.three) ** 2))
+        a = math.sqrt(((self.three - self.two) ** 2) + ((self.three - self.two) ** 2))
+        b = math.sqrt(((self.four - self.one) ** 2) + ((self.four - self.one) ** 2))
+        return a + b + c + d
+        print("Периметр:", P)
+
+    def area(self):
+        c = math.sqrt(((self.two - self.one) ** 2) + ((self.two - self.one) ** 2))
+        d = math.sqrt(((self.four - self.three) ** 2) + ((self.four - self.three) ** 2))
+        a = math.sqrt(((self.three - self.two) ** 2) + ((self.three - self.two) ** 2))
+        b = math.sqrt(((self.four - self.one) ** 2) + ((self.four - self.one) ** 2))
+        return ((a + b) / 2) * (math.sqrt((c ** 2) - ((((b - a) ** 2) + (c ** 2) - (d ** 2)) / (2 * (b - a)))))
+        print("Площадь:", S, "\n")
+
+
+trap1 = Trap(5, 7, 2, 2)
+trap1.proverka_if()
+trap1.side()
+print("Периметр: ", trap1.perimeter())
+print("Площадь: ", trap1.area())
