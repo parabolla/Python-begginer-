39582b250dec3a1d449b8ffd757c65b773ac3c2d
Iliya Tkachev <xarament@gmail.com>
2022-10-19 18:01:27 +0400

    not completed hw7_normal. #1

--- lesson 7/hw07_normal.py
+++ lesson 7/hw07_normal.py
@@ -16,37 +16,38 @@
 # 4. Узнать ФИО родителей указанного ученика
 # 5. Получить список всех Учителей, преподающих в указанном классе
 
-class Number_class:
-    def __init__(self, num_let=None):  # номер класса и буквы
-        # self.number = number
-        # self.letter = letter
-        self.num_let = num_let  # наследовать
-
-    # def num_let1(self):
-    #     self.num_let = str(self.number) + self.letter
-
-
-class Obj:
-    def __init__(self, obj):  # наименование предметов
-        self.obj = obj
-
-
-class Teachers:
-    def __init__(self, surname, obj="all", num_let=None):
-        self.surname = surname
-        self.obj = obj  # наследовать из Obj
-        self.num_let = num_let  # наследовать из Number_class
-
-
-# Должен быть как главный класс Родитель
-class Students(Obj, Number_class):
-    def __init__(self, name, surname, teachers_name, parents, num_let, obj):
+class School:
+    def __init__(self, num_let=None, obj=None, name=None, surname=None):
+        self.num_let = num_let  # наименование классов школы
+        self.obj = obj  # предметы
         self.name = name
         self.surname = surname
-        self.teachers_name = teachers_name
-        self.parents = parents
-        super(Obj, self).__init__(obj)
-        super(Number_class, self).__init__(num_let)
 
-    def full_name(self):
-        return self.name[0] + "." + self.surname[0]
+
+class Teachers(School):
+    def __init__(self, surname, obj, num_let):
+        super(Teachers, self).__init__(surname)
+        super(Teachers, self).__init__(obj)  # предметы
+        super(Teachers, self).__init__(num_let)  # наименование классов школы
+
+
+class Students(School):
+    def __init__(self, name, surname, parent_m, parent_f, num_let):
+        super(Students, self).__init__(name)
+        super(Students, self).__init__(surname)
+        self.parent_m = parent_m
+        self.parent_f = parent_f
+        super(Students, self).__init__(num_let)  # наименование классов школы
+
+
+obj1 = School("math")
+obj2 = School("rus_lang")
+
+num_let1 = School("5V")
+num_len2 = School("5K")
+
+Teacher1 = Teachers("Ivanov", obj1, num_let1)
+Teacher2 = Teachers("Petrov", [obj1, obj2], [num_let1, num_len2])
+
+Pupil1 = Students("Max", "Sidorov", "Sidorova V.V.", "Sidorov M.P.", num_let1)
+Pupil2 = Students("Alex", "Antonov", "Antonova D.D.", "Antonov S.Q.", num_let1)
