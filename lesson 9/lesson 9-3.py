5b6290d96131b3f8cf04d7186beccdbb0c8af54b
Iliya Tkachev <xarament@gmail.com>
2022-10-26 23:59:53 +0400

    try fabric metod

---
+++ lesson 9/lesson 9-3.py
@@ -0,0 +1,40 @@
+class Image():
+    def __init__(self, img):
+        self.img = img
+
+    def get(self):
+        return "<int {} >".format(self.img)
+
+
+class Input():
+    def __init__(self, inp):
+        self.inp = inp
+
+    def get(self):
+        return "<input {} >".format(self.inp)
+
+
+class Text():
+    def __init__(self, text):
+        self.text = text
+
+    def get(self):
+        return "<p>{}</p>".format(self.text)
+
+
+class Link():
+    def __init__(self, link):
+        self.link = link
+
+    def get(self):
+        return "<link {}>".format(self.link)
+
+
+img1 = Image("src=img_girl.jpg")
+inp1 = Input("type=text")
+text1 = Text("This is some text in a paragraph.")
+lint1 = Link("rel=stylesheet")
+
+tag = [img1, inp1, text1, lint1]
+for t in tag:
+    print(t.get())
