e3be2248a79c4c7293280a3052874752135fc988
Iliya Tkachev <xarament@gmail.com>
2022-10-31 20:32:11 +0400

    hw_10

--- lesson 10/hw_10.py
+++ lesson 10/hw_10.py
@@ -1,46 +1,53 @@
 # Enterprise
+import random
+import random
+from datetime import date
+
+
 class People:
     def __init__(self, surname=str, name=str, birthday=str, telnum=int):
 
-        self.__surname = surname
-        self.__name = name
-        self.__birthday = birthday
-        self.__telnum = telnum  # телефонный номер
-
-        @property
-        def telnum(self):
-            return self.__telnum
-
-        @telnum.setter
-        def telnum(self, value):
-            if len(str(value)) < 11:
-                print("Нужно ввести 11 значное число")
-            else:
-                return self.__telnum
+        self.surname = surname
+        self.name = name
+        self.birthday = birthday
+        self.telnum = telnum  # телефонный номер
+        if len(str(self.telnum)) == 11:
+            self.telnum
+        else:
+            raise print("ошибка")
 
 
 class Employee(People):
-    def __init__(self, surname, name, birthday, telnum, pernum=int, dpcode=int, salary=int):
+    def __init__(self, surname, name, birthday, telnum, pernum=int(0), dpcode=int(0), salary=int(0)):
         super().__init__(surname, name, birthday, telnum)
         self.__pernum = pernum  # id
         self.__dpcode = dpcode  # code
         self.__salary = salary  # оклад
 
     @property
-    def salary(self):
-        return self.__salary
-
-    @salary.setter
-    def salary(self, value):
-        self.__salary = value
+    def pernum(self):
+        return self.__pernum
 
     @property
     def dpcode(self):
         return self.__dpcode
 
-    @dpcode.setter
+    @property
+    def salary(self):
+        return self.__salary
+
+    @salary.setter  # зп
+    def salary(self, value):
+        self.__salary = value
+
+    @salary.setter  # автомазитизируем выдачу рандомных id
     def dpcode(self, value):
-        return value
+        self.__dpcode = value
+
+    @salary.setter  # соединяем id и уникальный табельный номер
+    def pernum(self, value):
+        value += self.dpcode
+        self.__pernum = value
 
     @dpcode.deleter
     def dpcode(self):  # удаляем заводской код и оклад
@@ -48,17 +55,28 @@ class Employee(People):
         self.__salary = "уволен"
 
 
+#
 emp1 = Employee("Petrov",
                 "Petr",
-                "24.3.2024",
-                89232432030,
-                1,
-                22133,
-                200)
+                2014 - 10 - 10,
+                89232435123,
+                1)
+#
+emp2 = Employee("Ivanov",
+                "Ivan",
+                2010 - 9 - 12,
+                89232435522,
+                2)
 
-# print(emp1.telnum, emp1.birthday)
+a = emp1.dpcode = 1
+print(a)
+b = emp1.pernum = 2
+print(b)
+# emp1.pernum(2)
+
 # del emp1.dpcode
-# print(emp1.salary)
+# print(emp1.pernum)
+# print(emp2.pernum)
 # print(emp1.dpcode)
-emp1.salary = 400  # меняем оклад
-print(emp1.salary)
+# emp1.salary = 400  # меняем оклад
+# print(emp1.salary)
