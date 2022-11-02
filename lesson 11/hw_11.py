654eea8ca3c89b97f5437ab3b7fb126307f4355d
Iliya Tkachev <xarament@gmail.com>
2022-11-02 22:11:12 +0400

    hw_11 completed #1

---
+++ lesson 11/hw_11.py
@@ -0,0 +1,69 @@
+import threading
+import time
+from threading import Event
+
+lock = threading.RLock()
+event = Event()
+
+
+class Writer1(threading.Thread):
+    def __init__(self, interval, daemon=True):
+        super().__init__()
+        self.interval = interval
+        self.daemon = daemon
+
+    def run(self):
+        while True:
+            event.set()
+            lock.acquire()  # блокируем
+            file = open("txt.txt", "a")
+            file.write("строка2")
+            print("первый автор отписался")
+            file.close()
+            lock.release()  # разблокировка
+            time.sleep(self.interval)
+
+
+class Writer2(threading.Thread):
+    def __init__(self, interval, daemon=True):
+        super().__init__()
+        self.interval = interval
+        self.daemon = daemon
+
+    def run(self):
+        while True:
+            event.set()
+            lock.acquire()
+            file = open("txt.txt", "a")
+            file.write("строка3")
+            print("второй автор отписался")
+            file.close()
+            lock.release()
+            time.sleep(self.interval)
+
+
+class Readers(threading.Thread):
+    def __init__(self, daemon=True):
+        super().__init__()
+        self.daemon = daemon
+
+    def run(self):
+        while True:
+            event.wait()
+            event.set()
+            lock.acquire()
+            file = open("txt.txt", "a")
+            print("мы все прочитали")
+            file.close()
+            lock.release()
+            event.clear()
+
+
+if __name__ == "__main__":
+    t1 = Writer1(1)
+    t2 = Writer2(1)
+    r = Readers()
+    t1.start()
+    t2.start()
+    r.start()
+    time.sleep(5)  # количетсво записей
