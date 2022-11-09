6d735e0eb900df5c9259d5bd14cab34c0841d0f5
Iliya Tkachev <xarament@gmail.com>
2022-11-09 21:22:29 +0400

    new_server fix

--- lesson 12/server_tcp.py
+++ lesson 12/server_tcp.py
@@ -1,7 +1,9 @@
 import socket
 import json
 from threading import Event
+import threading
 
+lock = threading.RLock()
 event = Event()
 
 
@@ -14,18 +16,23 @@ def reception():
     server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_sock.bind(("localhost", 9999))
     server_sock.listen(5)
+    users = []
 
     while True:
-        event.set()
         client_socker, addr = server_sock.accept()  # получаем от клиента(кортеж)
         request = client_socker.recv(1024)  # получение данных
+        users.append(addr)
+        print("Список подлючений {}".format(users))
         print(request.decode("utf-8"))
         print()
-        print(addr)
-        event.wait()
-        client_socker.sendall(request)  # отправляем сообщение обратно
+        print("здесь адресс {}".format(addr))
+        for user in users:
+            print(user)
+            if user[0] != addr[0]:
+                client_socker.sendto(request, user)
+
+        # client_socker.sendall(request)  # отправляем сообщение обратно
         client_socker.close()
-        event.clear()
 
 
 if __name__ == '__main__':
