116096d2d3ab8e3b2d8c028d7015ae8d2484e024
Iliya Tkachev <xarament@gmail.com>
2023-02-23 19:43:57 +0400

    Рабочая основа

---
+++ game/sea_battle.py
@@ -0,0 +1,31 @@
+import random
+
+# Создаем доску с кораблями
+horizon = ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
+vertical = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+counter = 5
+places = []
+while counter > 0:  # раскидываем корабли по игровой зоне
+    counter -= 1
+    if places.append([horizon[random.randint(0, 8)], vertical[random.randint(0, 8)]]) in places:
+        places.pop
+    else:
+        places.append([horizon[random.randint(0, 8)], vertical[random.randint(0, 8)]])
+print(places)  # Убираем, чтобы не было подсказки где расположенны корабли
+
+
+def game(): #начинает игру
+    while len(places) > 0:
+        dot = input("Введите точку") #Вводим точку B1
+        if list(dot) in places:
+            print(f"корабль потоплен на {list(dot)}")
+            places.remove(list(dot))
+            print(places)
+        else:
+            print("мимо")
+
+
+game()
+
+if __name__ == "__main__":
+    game()
